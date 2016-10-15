2573f6099c20d25a836a5bcc25e7a5cc99a72bc0
JinoWu <jwu29@stevens.edu>
2016-10-14 22:43:49 -0400

    Simple UI

---
+++ WeTunes-UI/WeTunesProtp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeTunesProtp
+//
+//  Created by Jino Wu on 10/14/16.
+//  Copyright © 2016 Jino Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
